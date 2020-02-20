2e2e70b2c3a38886c6f7e75cdf7369e935def86f
Rui Vieira <ruidevieira@googlemail.com>
2020-02-20 22:48:45 +0000

    Add trained model

---
+++ Model.py
@@ -0,0 +1,11 @@
+from sklearn.externals import joblib
+
+class Model(object):
+
+    def __init__(self):
+        print("Initializing.")
+        print("Loading model.")
+        self.model = joblib.load('model.pkl')
+
+    def predict(self, X, features_names):
+        return self.model.predict_proba(X)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ccfd-seldon-model
+
+Seldon model trained with the Kaggle credit card fraud dataset.
+
+To build the model:
+
+```shell
+$ s2i build https://github.com/ruivieira/ccfd-seldon-model.git seldonio/seldon-core-s2i-python3 ruivieira/ccfd-seldon-model
+```
